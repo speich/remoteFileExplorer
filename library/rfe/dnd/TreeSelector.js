6dffedb2674c6201753c71180a630d28014aa58c
Simon Speich <simon.speich@gmail.com>
2011-09-20 22:46:57 +0200

    do not select a node when only dragging it do not deselect previously selected node when dragging

--- library/rfe/dnd/TreeSelector.js
+++ library/rfe/dnd/TreeSelector.js
@@ -1,65 +1,95 @@
+/**
+ * So far the sole purpose of extending the _dndSelector are two things:
+ * 1. Prevent deselecting the last selected node when dragging a node different from the last selected one
+ * 2. Do not select a node when only dragging it
+ * @see conversation on http://dojo-toolkit.33424.n3.nabble.com/Reason-behind-selecting-treeNode-onMouseDown-and-not-onMouseUp-td3021149.html#a3066447
+ *
+ * Solution: The idea is to defer de-/selection on mouseDown to mouseUp (@see setSelection())
+ */
+
 define([
 	'dojo/_base/array',
 	'dojo/_base/lang',
+	'dojo/_base/event',
+	'dojo/_base/connect',
+	'dojo/mouse',
 	'original/dijit/tree/_dndSelector'
-], function(array, lang, _dndSelector) {
+], function(array, lang, event, connect, mouse, _dndSelector) {
 
 	// set references to call be able to call overriden methods
 	var ref = _dndSelector.prototype;
-	var oldMouseDown = ref.onMouseDown;
 	var oldMouseUp = ref.onMouseUp;
-	var oldMouseMove = ref.onMouseMove;
-
 
 	// TODO: make right click (context menu) select the tree node (also see dnd/GridSelector.js)
 	return _dndSelector.extend({
-		_markedNode: null,
-		_doMarkNode: false,
-		getSelectedNodes: ref.getSelectedTreeNodes,  // TODO: why doesn' tree._dndSelector not use this instead?
+		_oldSelection: [],
 
-		onMouseDown: function(e) {
-			oldMouseDown.apply(this, arguments);
-			this._doMarkNode = true;
-		},
+		_selectByMouse: false,
+		getSelectedNodes: ref.getSelectedTreeNodes,	// map two dnd method
 
-		onMouseUp: function(e) {
-			// Prevent selecting onMouseDown -> move to onMouseUp, but not when dragging (-> set to false onMouseMove())
-			if (this._doMarkNode) {
-				var selection = this.getSelectedTreeNodes();
-				var i = 0, len = selection.length;
-				if (this._markedNode) {
-					this._markedNode.setSelected(false);
-				}
-				for (; i < len; i++) {
-					selection[i].setSelected(true);
-				}
-				this._markedNode = this.current;
+		onMouseDown: function(evt) {
+			this._selectByMouse = true;
+
+			// ignore click on expando node
+			if(!this.current || this.tree.isExpandoNode(evt.target, this.current)){ return; }
+
+			event.stop(evt);
+
+			var treeNode = this.current,
+			  copy = connect.isCopyKey(evt), id = treeNode.id;
+
+			// if shift key is not pressed, and the node is already in the selection, delay deselection until mouseUp
+			// -> in the case of DND, deselection will be canceled by mouseMove.
+			if (!this.singular && !evt.shiftKey && this.selection[id]) {
+				this._doDeselect = true;
+				return;
+			} else {
+				this._doDeselect = false;
 			}
-			this._doMarkNode = false;
-			oldMouseUp.apply(this, arguments);
+			this.userSelect(treeNode, copy, evt.shiftKey);
 		},
 
-		onMouseMove: function(e) {
-			oldMouseMove.apply(this, arguments);
-			this._doMarkNode = false;
+		onMouseUp: function(evt) {
+			// selecting/deselecting does not work correctly when in multiselect mode after dragging unselected node
+			oldMouseUp.apply(this, arguments);
+			// Prevent selecting onMouseDown -> move to onMouseUp, but not when dragging (-> set to false onMouseMove())
+			var selection = this._oldSelection;
+			var i = 0, len = selection.length;
+			for (; i < len; i++) {
+				selection[i].setSelected(false);
+			}
+			selection = this.getSelectedTreeNodes();
+			i = 0, len = selection.length;
+			for (; i < len; i++) {
+				selection[i].setSelected(true);
+			}
+			this._oldSelection = selection;
+			this._selectByMouse = false;
 		},
 
 		setSelection: function(newSelection) {
+			// note: Parent method does two things: Add/remove nodes from/to selection and set them selected.
+			// To make 1. and 2. (see class comment) work, this should be rewritten and split into two separated methods.
+			// Unfortunately that's not possible since this method is also used by other methods, such as tree.set('path').
+			// Therefore we use a flag when using it with the mouse and thne defer the selecting to mouseUp
 			var oldSelection = this.getSelectedTreeNodes();
-			array.forEach(this._setDifference(oldSelection, newSelection), lang.hitch(this, function(node) {
-				if (this._markedNode != node) {
+			array.forEach(this._setDifference(oldSelection, newSelection), lang.hitch(this, function(node){
+				if (!this._selectByMouse) {
 					node.setSelected(false);
 				}
-				if (this.anchor == node) {
+				if(this.anchor == node){
 					delete this.anchor;
 				}
 				delete this.selection[node.id];
 			}));
-			array.forEach(this._setDifference(newSelection, oldSelection), lang.hitch(this, function(node) {
+			array.forEach(this._setDifference(newSelection, oldSelection), lang.hitch(this, function(node){
+				if (!this._selectByMouse) {
+					node.setSelected(true);
+					this._oldSelection.push(node);
+				}
 				this.selection[node.id] = node;
 			}));
 			this._updateSelectionProperties();
 		}
 	});
-
 });
