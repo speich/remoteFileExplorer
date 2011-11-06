9167496c0117b0cccae4f755e97fcd7c39cf4d36
Simon Speich <simon.speich@gmail.com>
2011-11-06 19:23:35 +0100

    fixed do not select node after dragging

--- library/rfe/dnd/TreeSelector.js
+++ library/rfe/dnd/TreeSelector.js
@@ -20,14 +20,17 @@ define([
 	// set references to call be able to call overriden methods
 	var ref = _dndSelector.prototype;
 	var oldMouseUp = ref.onMouseUp;
+	var oldMouseMove = ref.onMouseMove;
 
 	// TODO: make right click (context menu) select the tree node (also see dnd/GridSelector.js)
 	return _dndSelector.extend({
 		_oldSelection: [],
 		_selectByMouse: false,
+		_dragged: false,
 		getSelectedNodes: ref.getSelectedTreeNodes, // map two dnd method
 
 		/*** add state for selection to tree until patch http://bugs.dojotoolkit.org/ticket/14058 is checked in ***/
+		/*
 		_updateSelectionProperties: function(){
 			var selected = this.getSelectedTreeNodes();
 			var paths = [], nodes = [], selects = [];
@@ -51,12 +54,14 @@ define([
 	         cookie(this.cookieName, selects.join(","), {expires:365});
          }
 		},
+		*/
 		/*** end patch ***/
 
 		onMouseDown: function(evt) {
-			// note: Overriding to remove doing nothing on right click and also to remove stopping event (we need to bubble up
+			// Note: Overriding to remove doing nothing on right click and also to remove stopping event (we need to bubble up
 			// to know where user clicked at in FileExplorer.getWidget
 			this._selectByMouse = true;
+			this._dragged = false;
 
 			// ignore click on expando node
 			if(!this.current || this.tree.isExpandoNode(evt.target, this.current)){ return; }
@@ -66,41 +71,56 @@ define([
 			var treeNode = this.current,
 			  copy = connect.isCopyKey(evt), id = treeNode.id;
 
-			// if shift key is not pressed, and the node is already in the selection, delay deselection until mouseUp
-			// -> in the case of DND, deselection will be canceled by mouseMove.
+			// _doDeselect is the flag to indicate that the user wants to either ctrl+click on
+			// a already selected item (to deselect the item), or click on a not-yet selected item
+			// (which should remove all current selection, and add the clicked item). This can not
+			// be done in onMouseDown, because the user may start a drag after mousedown. By moving
+			// the deselection logic here, the user can drag an already selected item.
 			if (!this.singular && !evt.shiftKey && this.selection[id]) {
 				this._doDeselect = true;
 				return;
 			} else {
 				this._doDeselect = false;
 			}
-			this.userSelect(treeNode, copy, evt.shiftKey);
+			this.userSelect(treeNode, copy, evt.shiftKey);	// add node to selection and calls setSelection()
+		},
+
+		onMouseMove: function() {
+			oldMouseMove.apply(this, arguments);
+			this._dragged = true;
 		},
 
 		onMouseUp: function(evt) {
+			var i, len, selection;
+
 			// selecting/deselecting does not work correctly when in multiselect mode after dragging unselected node
 			oldMouseUp.apply(this, arguments);
 			// Prevent selecting onMouseDown -> move to onMouseUp, but not when dragging (-> set to false onMouseMove())
-			var selection = this._oldSelection;
-			var i = 0, len = selection.length;
-			for (; i < len; i++) {
-				selection[i].setSelected(false);
+			if (!this._dragged) {	// set to false on mousemove
+				selection = this._oldSelection;
+				i = 0;
+				len = selection.length;
+				for (; i < len; i++) {
+					selection[i].setSelected(false);
+				}
+				selection = this.getSelectedTreeNodes();
+				i = 0;
+				len = selection.length;
+				for (; i < len; i++) {
+					selection[i].setSelected(true);
+				}
+				this._oldSelection = selection;
 			}
-			selection = this.getSelectedTreeNodes();
-			i = 0;
-            len = selection.length;
-			for (; i < len; i++) {
-				selection[i].setSelected(true);
-			}
-			this._oldSelection = selection;
 			this._selectByMouse = false;
+			this._dragged = false;
 		},
 
 		setSelection: function(newSelection) {
-			// note: Parent method does two things: Add/remove nodes from/to selection and set them selected.
+			// Note: Overriden (parent) method would do two things: Add/remove nodes from/to selection and set them selected.
 			// To make 1. and 2. (see class comment) work, this should be rewritten and split into two separated methods.
 			// Unfortunately that's not possible since this method is also used by other methods, such as tree.set('path').
-			// Therefore we use a flag when using it with the mouse and then defer the selecting to mouseUp
+			// Therefore we use a flag when using it with the mouse, which will skip selecting/deselecting a node here, so we
+			// can do it in mouseUp if there was no drag in between
 			var oldSelection = this.getSelectedTreeNodes();
 			array.forEach(this._setDifference(oldSelection, newSelection), lang.hitch(this, function(node){
 				if (!this._selectByMouse) {
