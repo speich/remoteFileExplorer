ce7bb0d3f80f0169f2c5d8878b9150106456a751
Simon Speich <simon.speich@gmail.com>
2012-02-29 17:06:32 +0100

    work in progress on own avatar

---
+++ library/rfe/dnd/Manager.js
@@ -0,0 +1,7 @@
+define(['dojo/_base/declare', 'original/dojo/dnd/Manager', 'dojo/dnd/Avatar'], function(declare, Manager, Avatar) {
+	return Manager; /*.extend({
+		makeAvatar: function() {
+			return new Avatar(this);
+		}
+	});*/
+});
