70e475fbcdd994210305f34c9cf2c05ac4cec766
Simon Speich <simon.speich@gmail.com>
2012-12-22 11:25:54 +0100

    replace console.js with Console.js

---
+++ library/rfe/Console.js
@@ -0,0 +1,138 @@
+define([
+	'dojo/_base/declare',
+	'dojo/_base/lang',
+	'dijit/_WidgetBase',
+	'dojo/request/notify',
+	'dojo/dom',
+	'dojo/dom-construct',
+	'dojo/json',
+	'dojo/date/locale'
+], function(declare, lang, _WidgetBase, notify, dom, domConstruct, json, locale) {
+
+	return declare([_WidgetBase], {
+	//return declare(null, {
+
+		baseClass: 'rfeConsole',
+
+		logOwnMessagesOnly: true,
+
+		constructor: function(/* Object */args, /* DOMNode */ node) {
+			this.domNode = node;
+			notify('done', lang.hitch(this, this.log));
+		},
+
+
+		buildRendering: function() {
+			this.inherited('buildRendering', arguments);
+
+			// create the DOM for this widget
+			this.domNode.innerHTML = '<label>log:</label>';
+
+			domConstruct.create('button', {
+				'class': 'buttIcon',
+				title: 'reset log',	// TODO: use i18n
+				onclick: lang.hitch(this, function(evt) {
+					evt.preventDefault();
+					this.clear();
+					return false;
+				}),
+				innerHTML: '<img src="' + require.toUrl('rfe/resources/images/icon_reset.png') + '" alt="reset icon" title="reset">'
+			}, this.domNode);
+
+			domConstruct.create('div', {
+				innerHTML: '<div></div>'
+			}, this.domNode);
+
+			this.containerNode = domConstruct.create('ul', null, this.domNode);
+
+		},
+
+
+		/**
+		 * Write own message or io error message to log container.
+		 * @param {String} response message
+		 * @param {Boolean} [isErr] is message an error message
+		 */
+		log: function(response, isErr) {
+			var msg, responseObj;
+
+			isErr = isErr || false;
+
+			if (typeof response === 'string') {
+				// direct log message
+				this.print(response);
+				return;
+			}
+
+			// request has failed
+			if (response instanceof Error) {
+				responseObj = response.response;
+				isErr = true;
+			}
+			// request has succeeded
+			else {
+				responseObj = response;
+			}
+
+			msg = responseObj.xhr.statusText + ': ' + responseObj.options.method + ' ' + decodeURI(responseObj.url);
+			msg += this.extractMsg(responseObj.text);
+
+			this.print(msg, isErr, responseObj.options.method);
+		},
+
+		/**
+		 * Extract own messages from response body.
+		 * @param {String} response as json
+		 * @return {String}
+		 */
+		extractMsg: function(response) {
+			var i, len, msg = '',
+				result = json.parse(response);
+
+			if (result instanceof Array) {
+				len = result.length;
+				for (i = 0; i < len; i++) {
+					if (result[i].msg) {
+						msg += '<br>' + result[i].msg;
+					}
+				}
+			}
+			return msg;
+		},
+
+		/**
+		 * Print message to HTMLDivElement.
+		 * If isErr is set to true, message is printed in red.
+		 * If request method is not GET and not an error, message is printed in green.
+		 * @param {String} msg message to print
+		 * @param {Boolean} isErr is message an error?
+		 * @param {String} reqMethod request method
+		 */
+		print: function(msg, isErr, reqMethod) {
+			var date, cl = 'logMsg';
+
+			date = locale.format(new Date(), {
+				selector: 'time',
+				timePattern: 'HH:mm:ss'
+			});
+			if (isErr) {
+				cl += ' logErrMsg';
+			}
+			else if (reqMethod !== 'GET') {
+				cl += ' logSuccessMsg';			}
+
+			domConstruct.create('li', {
+				'class': cl,
+				innerHTML: date + ' ' + msg + '<br/>'
+			}, this.containerNode, 'first');
+		},
+
+		/**
+		 * Clear all logged messages.
+		 */
+		clear: function() {
+			this.containerNode.innerHTML = '';
+		}
+
+	});
+});
