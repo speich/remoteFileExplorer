19eb5e7549e874c16ec8c13b0457ccec14db2583
Simon Speich <simon.speich@gmail.com>
2013-09-08 15:44:32 +0200

    added some screenshots

--- php/inc_global.php
+++ php/inc_global.php
@@ -13,7 +13,7 @@ set_time_limit($timeout);
 date_default_timezone_set('Europe/Zurich');
 
 // If you want to work on different servers and subdirectories set this path
-$path = '/';
+$path = '/library/remoteFileExplorer/';
 
 // make include paths available to pages independent on subdir they reside in. On windows this has to be absolute paths
 $absPath = rtrim($_SERVER['DOCUMENT_ROOT'], '/').$path;
