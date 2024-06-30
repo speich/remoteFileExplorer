da8a28aa11294fdfd3fd1d74e14aeeb8914b4aa5
Simon <info@speich.net>
2024-06-30 12:49:48 +0200

    bump all dependencies to latest versions

--- rfe.php
+++ rfe.php
@@ -1,53 +1,53 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
 <head>
-<meta charset="UTF-8">
-<title>Remote File Explorer (rfe)</title>
-<link rel="stylesheet" type="text/css" href="/library/dojo/1.17.3/dijit/themes/claro/document.css">
-<link rel="stylesheet" type="text/css" href="/library/dojo/1.17.3/dijit/themes/claro/claro.css">
-<link rel="stylesheet" href="/library/dgrid/0.3.21/css/skins/claro.css">
-<link rel="stylesheet" href="/js/resources/reset.css">
-<link rel="stylesheet" href="/js/resources/rfe.css">
+    <meta charset="UTF-8">
+    <title>Remote File Explorer (rfe)</title>
+    <link rel="stylesheet" type="text/css" href="/library/dojo/1.17.3/dijit/themes/claro/document.css">
+    <link rel="stylesheet" type="text/css" href="/library/dojo/1.17.3/dijit/themes/claro/claro.css">
+    <link rel="stylesheet" href="/library/dgrid/0.3.21/css/skins/claro.css">
+    <link rel="stylesheet" href="/js/resources/reset.css">
+    <link rel="stylesheet" href="/js/resources/rfe.css">
 </head>
 
 <body class="claro rfe">
 <div id="remoteFileExplorer" class="rfe"></div>
 <script type="text/javascript">
-var dojoConfig = {
-	async: true,
-	baseUrl: '/library/dojo/1.17.3',
-	packages: [
-		{name: 'dojo', location: 'dojo'},
-		{name: 'dijit', location: 'dijit'},
-		{name: 'dgrid', location: '../../dgrid/0.3.21'},  /* this is the last (known) working version */
-		{name: 'xstyle', location: '../../xstyle/0.3.3'},
-		{name: 'put-selector', location: '../../put-selector/0.3.6'},
-		{name: 'rfe', location: '../../../js'},
-		{name: 'rfe-php', location: '../../../php'}
-	],
-	map: {
-		// redirect the following modules to my own modules
-		'dijit/tree': {
-			'dijit/tree/_dndSelector': 'rfe/dnd/TreeSelector',
-			'dijit/tree/dndSource': 'rfe/dnd/TreeSource'
-		},
-		'dojo/dnd': {
-			'dojo/dnd/Selector': 'rfe/dnd/Selector'
-		}
-	}
-};
+  var dojoConfig = {
+    async: true,
+    baseUrl: '/library/dojo/1.17.3',
+    packages: [
+      {name: 'dojo', location: 'dojo'},
+      {name: 'dijit', location: 'dijit'},
+      {name: 'dgrid', location: '../../dgrid/0.3.21'},  /* this is the last (known) working version */
+      {name: 'xstyle', location: '../../xstyle/0.3.3'},
+      {name: 'put-selector', location: '../../put-selector/0.3.6'},
+      {name: 'rfe', location: '../../../js'},
+      {name: 'rfe-php', location: '../../../php'}
+    ],
+    map: {
+      // redirect the following modules to my own modules
+      'dijit/tree': {
+        'dijit/tree/_dndSelector': 'rfe/dnd/TreeSelector',
+        'dijit/tree/dndSource': 'rfe/dnd/TreeSource'
+      },
+      'dojo/dnd': {
+        'dojo/dnd/Selector': 'rfe/dnd/Selector'
+      }
+    }
+  };
 </script>
 <script src="/library/dojo/1.17.3/dojo/dojo.js"></script>
 <script type="text/javascript">
-require(['dojo/ready', 'rfe/FileExplorer'], function(ready, FileExplorer) {
-	ready(function() {
-		var rfe = new FileExplorer({
-			id: 'remoteFileExplorer',
-			origPageUrl: '/rfe.php'
-		});
-		rfe.startup();
-	});
-});
+  require(['dojo/ready', 'rfe/FileExplorer'], function(ready, FileExplorer) {
+    ready(function() {
+      var rfe = new FileExplorer({
+        id: 'remoteFileExplorer',
+        origPageUrl: '/rfe.php'
+      });
+      rfe.startup();
+    });
+  });
 </script>
 </body>
 </html>
